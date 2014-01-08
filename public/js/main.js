2b82c99acffb2f30b80dc5e05b55952a1a1ea08a
bbaumgar <bwb8ta@virginia.edu>
2014-01-08 16:00:35 -0500

    Create httpService to handle interactions with backend.

--- public/js/main.js
+++ public/js/main.js
@@ -2,17 +2,17 @@ angular.module('matcher', ['ui.bootstrap'])
     .controller('matcherForm', ['$scope', 'httpService', function ($scope, httpService) {
         $scope.fields = [
             {
-               title: "Computer Science",
-               focuses: [
-                   "Artificial Intelligence",
-                   "Genetic Algorithms",
-                   "Machine Vision",
-                   "Networking",
-                   "Formal Methods",
-                   "Databases",
-                   "Parallel Programming",
-                   "Security"
-               ]
+                title: "Computer Science",
+                focuses: [
+                    "Artificial Intelligence",
+                    "Genetic Algorithms",
+                    "Machine Vision",
+                    "Networking",
+                    "Formal Methods",
+                    "Databases",
+                    "Parallel Programming",
+                    "Security"
+                ]
             },
             {
                 title: "Biomedical Engineering",
@@ -29,7 +29,7 @@ angular.module('matcher', ['ui.bootstrap'])
 
         var interestsList = [];
 
-        for(var y = 0; y < $scope.fields.length; y++) {
+        for (var y = 0; y < $scope.fields.length; y++) {
             $scope.fields[y].id = makeFieldId($scope.fields[y].title, "");
             for (var x = 0; x < $scope.fields[y].focuses.length; x++)
                 $scope.fields[y].focuses[x] = {
@@ -43,39 +43,54 @@ angular.module('matcher', ['ui.bootstrap'])
             return fieldName.replace(" ", "-").toLowerCase() + focusName.replace(" ", "-").toLowerCase();
         }
 
-        $scope.fieldClick = function(field, $event){
+        $scope.fieldClick = function (field, $event) {
             var selected = event.currentTarget.checked;
             field.open = selected;
             $scope.updateInterest(field.id, selected);
             $event.stopPropagation();
         }
 
-        $scope.updateInterest = function(id, selected, $event) {
-            if(selected)
+        $scope.updateInterest = function (id, selected, $event) {
+            if (selected)
                 interestsList.push(id);
             else
-                interestsList = interestsList.filter(function(item) {
+                interestsList = interestsList.filter(function (item) {
                     return item !== id;
                 });
             console.log(interestsList);
         }
 
-        $scope.submitForm = function() {
-            console.log("clicked!");
-            if($scope.isProfessor)
+        $scope.submitForm = function () {
+            if ($scope.isProfessor)
                 httpService.storeProfessor($scope.name, $scope.email, $scope.research, interestsList);
             else
-                httpService.recommendStudent()
-            console.log($scope.fields);
+                httpService.createStudent($scope.name, $scope.email, interestsList)
         }
     }])
-    .factory('httpService', function() {
+    .factory('httpService', ['$http', function ($http) {
         var service = {};
 
-        service.storeProfessor = function(name, email, research, interests)  {
-            console.log(name, email, research, interests);
+        service.storeProfessor = function (name, email, research, interests) {
+            $http.post({
+                name: name,
+                email: email,
+                research: research,
+                interests: interests
+            }).success(function(data) {
+                    console.log(data);
+                })
+        }
+
+        service.createStudent = function(name, email, interests) {
+            $http.post({
+                name: name,
+                email: email,
+                interests: interests
+            }).success(function(data) {
+                    console.log(data);
+                })
         }
 
         return service;
-    });
+    }]);
 
