ba642a62a233cbeaddb1ad594692ddcccb7e91b9
bbaumgar <bwb8ta@virginia.edu>
2014-01-10 20:37:47 -0500

    Write ruby script to convert list of fields to json file

---
+++ slurp.rb
@@ -0,0 +1,21 @@
+in_file = File.open("fields.txt", "r")
+out_file = File.open("fields.json", "w")
+
+is_title = false
+first = true
+out = '[{"title":'
+in_file.readlines.each do |line|
+    line = line.strip!
+    if is_title
+        out = out [0..-2]
+        out += ']},{"title":'
+    end
+    out += '"' + line + '",' if line.length > 2
+    if is_title or first
+        out += '"focuses": ['
+        first = false
+    end
+    is_title = line.length < 2
+end
+out = out[0..-2] + ']}]'
+out_file.puts out 
